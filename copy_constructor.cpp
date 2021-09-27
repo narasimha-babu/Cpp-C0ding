d9a150ea8656fe79fbea3c2daa0fff067a2016f9
narasimha babu <karenarasimhababu2002gmail.com>
2021-09-27 16:49:32 +0530

    modified copy_constructor.cpp and copy_constructor.exe

--- copy_constructor.cpp
+++ copy_constructor.cpp
@@ -7,8 +7,10 @@ class Number{
 int a;
 
     public:
+    int d;
         Number(){
             a = 10;
+            d = 20;
         }
         Number(int num){
             a = num;
@@ -20,14 +22,20 @@ int a;
         }
         
         void display();
+        void display1();
+
 };
 
 void Number :: display(){
     cout<<"number of this object is "<<a<<endl;
 }
 
+void Number :: display1(){
+    cout<<"number of this object is "<<d<<endl;
+}
+
 int main(){
-Number x, y(3), z(45);
+Number x, y(3), z;
 //   z(45);
 
   x.display();
@@ -35,6 +43,8 @@ Number x, y(3), z(45);
   z.display();
   Number z2 = z;
    z2.display();
-   
+Number *n = new Number;
+ n->d = 675;
+ n->display1();
     return 0;
 }

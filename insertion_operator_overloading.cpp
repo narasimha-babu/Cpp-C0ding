319ff497b2d8f94c343795e216cdb01e852ca3c6
narasimha-babu <karenarasimhababu2002@gmail.com>
2021-09-29 23:05:10 +0530

    edited it

--- insertion_operator_overloading.cpp
+++ insertion_operator_overloading.cpp
@@ -14,14 +14,15 @@ complex :: complex(int r=0, int i=0){
     real = r; img = i;
 }
 
-ostream & operator<<(ostream &o,complex &x){
+ostream & operator<<(ostream &o,complex x){
   o<<x.real<<" + i"<<x.img<<endl;
   return o;
 }
 int main(){
 complex c1(34,56);
 
-cout<<c1;
+cout<<c1;              //
+operator<<(cout,c1);   //  cout<< is similar to operator<< from now
 
 
 return 0;

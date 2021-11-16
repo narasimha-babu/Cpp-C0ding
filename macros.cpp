6313aa8b325d8b8b5e097f5a4ea5efb0f1209315
K Narasimha Babu <karenarasimhababu2002@gmail.com>
2021-11-16 13:15:58 +0530

    uploaded via updated

--- macros.cpp
+++ macros.cpp
@@ -2,10 +2,16 @@
 
 using namespace std;
 
-
     #define PI 3.14
+
+    #define msg(x) #x
+    #define max(x,y) (x>y?x:y)
+
 int main(){
+    // x = "hello mowa";
     cout<<PI<<endl;
+    cout<<msg(hello mowa)<<endl;
+    cout<<max(10,4)<<endl;
 
 return 0;
 
